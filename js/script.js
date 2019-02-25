3953855995c35feeef53e918b81d12c40c00f03a
Mahugu <isaacbihengoto@gmail.com>
2019-02-25 08:21:58 +0300

    added the jquery code in js file

--- js/script.js
+++ js/script.js
@@ -12,4 +12,14 @@ $(document).ready(function() {
     var email = $("#email").val();
     $("#name").text(name);
   });
+  $("#radio form").submit(function(event) {
+    event.preventDefault();
+    var answerOne = parseInt($("input:radio[name=quizOne]:checked").val());
+    var answerTwo = parseInt($("input:radio[name=quizTwo]:checked").val());
+    var answerThree = parseInt($("input:radio[name=quizThree]:checked").val());
+    var answerFour = parseInt($("input:radio[name=quizFour]:checked").val());
+
+    $("#radio").hide();
+    $(".registerTwo").show();
+  });
 });
